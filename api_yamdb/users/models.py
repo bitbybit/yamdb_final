bb1435607893e04c086377f898423116cdef9a91
Павел Морозов <filimon.doors@gmail.com>
2022-04-12 10:27:33 +0300

    user model: add user roles, email field defined as unique

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'пользователь'),
+        ('moderator', 'модератор'),
+        ('admin', 'администратор'),
+    )
+    role = models.CharField(
+        max_length=25,
+        verbose_name='роль пользователя',
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+    email = models.EmailField(unique=True)
+
