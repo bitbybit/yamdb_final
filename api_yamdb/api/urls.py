57d3f768c577bfa10f3ea6ef20d545c40d62053b
Павел Морозов <filimon.doors@gmail.com>
2022-04-13 16:34:52 +0300

    implement endpoints users, users/{username}

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import UserViewSet
+
+router = SimpleRouter()
+router.register("users", UserViewSet)
+
+urlpatterns = [path("v1/", include(router.urls))]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import filters, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from reviews.models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "username"
+
+    @action(detail=True, methods=["get", "patch"])
+    def me(self, request):
+        user = self.request.user
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
