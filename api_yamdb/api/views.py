1d93b9f510292e1433db6db61450f1bdefe87d5c
Павел Морозов <filimon.doors@gmail.com>
2022-04-13 21:17:18 +0300

    code polish

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,14 +1,23 @@
 from rest_framework import filters, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
 from reviews.models import User
+
 from .serializers import UserSerializer
+
+"""
+TODO: после реализации аутентификации протестировать работу эндпоинта users/me,
+поле role должно быть доступно только при GET запросе, юзер не может менять
+роль. можно для PATCH запроса использовать отдельный сериализатор, в котором
+поле role будет отсутствовать.
+"""
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("username",)
     lookup_field = "username"
 
     @action(detail=True, methods=["get", "patch"])
