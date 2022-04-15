19a1a0d75ad22d95ba079e7ccda3f7549d06c655
vshkinder <shkinder123@gmail.com>
2022-04-15 12:56:55 +0300

    Genre model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -19,3 +19,27 @@ class User(AbstractUser):
         'Биография',
         blank=True,
     )
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField(blank=True)
+    genre = models.ManyToManyField(Genre)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        blank=True,
+        null=True,
+    )
